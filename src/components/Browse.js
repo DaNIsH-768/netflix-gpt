ffacec9545123aa44a3107e3663cbdaa408e6967
Danish Raina <danishraina768@gmail.com>
2025-04-11 21:25:38 +0100

    Created a seperate movie slice to store the movies data

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,7 @@
 import {useDispatch} from "react-redux";
 import {useEffect} from "react";
 import {options} from "../utils/API_KEY";
+import {setNowPlayingMovies} from "../utils/movieSlice";
 
 const Browse = () => {
     const dispatch = useDispatch();
@@ -8,6 +9,7 @@ const Browse = () => {
     const fetchNowPlayingMovies = async () => {
         const rawData = await fetch("https://api.themoviedb.org/3/movie/now_playing", options);
         const json_data = await rawData.json();
+        dispatch(setNowPlayingMovies(json_data.results));
         console.log(json_data.results);
     }
 
